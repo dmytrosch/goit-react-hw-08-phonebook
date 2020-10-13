1dc73b868536ddc05e319f5dd19b8afe9cf92478
Dmytro.Sch <dmytro.sch@list.ru>
2020-10-13 18:52:57 +0300

    Updates

---
+++ precache-manifest.758e51fd94ebf45deb6a5d357e05b49e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8e90c5276bc2eb370d13a364c25a5c",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "7fa9840f07574bd2e0ab",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b4a99075.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b4a99075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fa2d999a94c540914f",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.5f5f17f0.chunk.js"
+  },
+  {
+    "revision": "0bbde2b646e190ef38fc",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.3dc95f75.js"
+  }
+]);
