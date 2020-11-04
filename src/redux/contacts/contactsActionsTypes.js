bc986b1ae24088091a2b77cdc0c1bc880332565c
Dmytro.Sch <dmytro.sch@list.ru>
2020-11-05 01:39:21 +0200

    remove constants file

--- src/redux/contacts/contactsActionsTypes.js
+++
@@ -1,5 +0,0 @@
-const ADD_CONTACT = "contact/add";
-const REMOVE_CONTACT = "contact/remove";
-const FILTER_CONTACT = "contact/filter";
-
-export default { ADD_CONTACT, REMOVE_CONTACT, FILTER_CONTACT };
